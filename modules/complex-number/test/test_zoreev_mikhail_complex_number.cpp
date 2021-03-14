cf74757af1ae365c5720aecaa3cedff12b78f282
Mikhail-Zoreev <43145054+BFDestroyeer@users.noreply.github.com>
2021-03-14 15:12:04 +0300

    update tests

--- modules/complex-number/test/test_zoreev_mikhail_complex_number.cpp
+++ modules/complex-number/test/test_zoreev_mikhail_complex_number.cpp
@@ -2,21 +2,20 @@
 
 #include <gtest/gtest.h>
 
-#include <turple>
-
+#include <tuple>
 #include "include/complex_number.h"
 
-TEST(Zoreev_Mikhail_ComplexNumberTest, Division_By_Self_Give_One) {
-    double re = 6.0;
-    double im = 5.0;
-    double result_re = 1.0;
-    double result_im = 1.0;
-    ComplexNumber number(re, im);
+TEST(Zoreev_Mikhail_ComplexNumberTest, Multiplication_On_Pair_Give_Zero_Im) {
+    double re_1 = 4.0;
+    double im_1 = 1.0;
+    double re_2 = 4.0;
+    double im_2 = -1.0;
+    ComplexNumber number(re_1, im_1);
+    ComplexNumber pair_number(re_2, im_2);
 
-    ComplexNumber result = number / number;
-
-    ASSERT_DOUBLE_EQ(number.getRe(), result_re);
-    ASSERT_DOUBLE_EQ(number.getIm(), result_im);
+    ComplexNumber result = number * pair_number;
+    
+    ASSERT_DOUBLE_EQ(result.getIm(), 0.0);
 }
 
 TEST(Zoreev_Mikhail_ComplexNumberTest, Equality_Transitivity) {
@@ -46,24 +45,18 @@ TEST(Zoreev_Mikhail_ComplexNumberTest, Summ_With_Opposite_Give_Zero) {
     ASSERT_DOUBLE_EQ(result.getIm(), 0.0);
 }
 
-typedef testing::TestWithParam<std::turple<double, double>> Zoreev_Mikhail_ComplexNumberTest_Eq_Is_Opposite_To_Ineq
+typedef testing::TestWithParam<std::tuple<double, double, double, double>>
+    Zoreev_Mikhail_ComplexNumberTest_Parametrized;
 TEST(Zoreev_Mikhail_ComplexNumberTest, Equality_Is_Opposite_To_Inequality) {
-    double re_1 = std::get<0>(GetParam());
-    double im_1 = std::get<1>(GetParam());
-    double re_2 = std::get<2>(GetParam());
-    double im_2 = std::get<3>(GetParam());
+    double re_1 = 1.0;
+    double im_1 = 5.0;
+    double re_2 = 3.0;
+    double im_2 = 5.0;
     ComplexNumber number_1(re_1, im_1);
     ComplexNumber number_2(re_2, im_2);
 
     bool is_equal = number_1 == number_2;
     bool is_not_equal = number_1 != number_2;
 
-    ASSER_NE(is_equal, is_not_equal);
+    ASSERT_NE(is_equal, is_not_equal);
 }
-
-INSTANTIATE_TEST_CASE_P(/**/, Zoreev_Mikhail_ComplexNumberTest_Eq_Is_Opposite_To_Ineq, testing::Combine(
-  testing::Values(1.0, 5.0, 1.0, 5.0),
-  testing::Values(6.0, 3.0, 5.0, 7.0),
-  testing::Values(5.0, 1.0, 1.0, 5.0),
-  testing::Values(4.0, 4.0, 4.0, 4.0)
-));
