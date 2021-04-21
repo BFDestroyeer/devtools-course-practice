12306e90f51f6f38a2983a00cf6129abd4ad9614
Mikhail-Zoreev <mzoreev@gmail.com>
2021-04-21 11:38:54 +0300

    fix style

--- modules/radix_sort/include/radix_sort.h
+++ modules/radix_sort/include/radix_sort.h
@@ -7,12 +7,11 @@
 #include <stdexcept>
 namespace RadixSort {
     /**
-     * Sorts
-     * @param begin
-     * @param end
+     * Sorts vector elements in range [begin, end)
+     * @param begin Iterator to begin element
+     * @param end Iterator to end element
      */
     void radixSort(std::vector<int64_t>::iterator begin, std::vector<int64_t>::iterator end);
 }  // namespace RadixSort
-
 
 #endif //MODULES_RADIX_SORT_INCLUDE_RADIX_SORT_H_
